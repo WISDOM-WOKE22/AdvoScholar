bf87566308fb1a1687a8b1e487022a0abf2acb9e
WISDOM-WOKE22 <wisdomwokedev@gmail.com>
2024-02-16 20:31:31 +0100

    created auth pages

--- frontend/src/pages/auth/setPassword/index.tsx
+++ frontend/src/pages/auth/setPassword/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function Login(){
+export default function SetPassword(){
     return (
         <div className="h-screen flex justify-center sm:items-center bg-white">
             <div className="rounded-3xl w-full max-w-lg sm:border-color mt-10 sm:mt-0 p-4 sm:p-10 sm:border border-solid sm:border-[#EDEFF5]">
@@ -9,28 +9,28 @@ export default function Login(){
                 </div>
                 <div className="flex flex-col mt-6 gap-5">
                     <div className="flex flex-col">
-                        <span>School Url</span>
-                        <input
-                            type="text" 
-                            className="border border-solid border-[#EDEFF5] p-3 rounded-xl mt-1"
-                            placeholder="Enter school url"
-                        />
-                    </div>
-                    <div className="flex flex-col">
-                        <span>Password</span>
+                        <span>Enter Password</span>
                         <input
                             type="password" 
                             className="border border-solid border-[#EDEFF5] p-3 rounded-xl mt-1"
-                            placeholder="Enter school url"
+                            placeholder="Enter Admin password"
                         />
                     </div>
-                    <div className="text-right">
+                    <div className="flex flex-col">
+                        <span>Confirm Password</span>
+                        <input
+                            type="password" 
+                            className="border border-solid border-[#EDEFF5] p-3 rounded-xl mt-1"
+                            placeholder="Confirm Admin password"
+                        />
+                    </div>
+                    {/* <div className="text-right">
                         <Link to='/forgot_password'>
                             <p className="text-[#5573EF] cursor-pointer">Forgot your password?</p>
                         </Link>
-                    </div>
+                    </div> */}
                     <div>
-                        <button className="rounded-xl w-full text-[white] bg-[#5573EF] p-3">Log in</button>
+                        <button className="rounded-xl w-full text-[white] bg-[#5573EF] p-3">Set Password</button>
                     </div>
                 </div>
             </div>
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,6 +4,10 @@ import { type RouteObject } from "react-router-dom";
 const Index = lazy(() => import("@/pages/index"));
 const Notfound = lazy(() => import("@/pages/404"));
 const Login = lazy(() => import("@/pages/auth/login"))
+const VerifyEmail = lazy(() => import("@/pages/auth/verify"))
+const ForgotPasssword = lazy(() => import("@/pages/auth/forgotten"))
+const Signup = lazy(() => import("@/pages/auth/signup"))
+const SetPassword = lazy(() => import("@/pages/auth/setPassword"))
 
 export const routes: Array<RouteObject> = [
   {
@@ -19,6 +23,38 @@ export const routes: Array<RouteObject> = [
     element: (
       <Suspense>
         <Login />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/verify_email",
+    element: (
+      <Suspense>
+        <VerifyEmail />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/forgot_password",
+    element: (
+      <Suspense>
+        <ForgotPasssword />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/signup",
+    element: (
+      <Suspense>
+        <Signup />
+      </Suspense>
+    ),
+  },
+  {
+    path: "set_password",
+    element: (
+      <Suspense>
+        <SetPassword />
       </Suspense>
     ),
   },
